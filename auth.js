2a64bb6da7cc8e4aa3d77cdc3a78b942b8e10b82
Ser-gg10 <134311478+Ser-gg10@users.noreply.github.com>
2026-02-09 16:53:52 +0100

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,23 +1,20 @@
 const ADMIN_PASS = 'sanchezcup';
 
-// Iniciar sesión como admin
 function loginAdmin(){
   const p = prompt('Contraseña admin');
   if(p === ADMIN_PASS){
-    localStorage.setItem('sc_role','admin'); // Guardamos rol admin
+    localStorage.setItem('sc_role','admin');
     alert('Modo admin activado');
-    location.reload(); // recarga para mostrar botones
+    location.reload();
   } else {
     alert('Contraseña incorrecta');
   }
 }
 
-// Comprobar si el usuario es admin
 function isAdmin(){
   return localStorage.getItem('sc_role') === 'admin';
 }
 
-// Modo oscuro
 function toggleDark(){
   document.body.classList.toggle('dark');
   localStorage.setItem('sc_dark', document.body.classList.contains('dark') ? '1' : '0');
