a943fbfc5ef50099643e808a4782e9d10dc0522a
Ser-gg10 <134311478+Ser-gg10@users.noreply.github.com>
2026-02-09 14:43:14 +0100

    Update data.js

--- data.js
+++ data.js
@@ -1,18 +1,14 @@
-// --- Helpers para LocalStorage ---
 function LS(k){ return JSON.parse(localStorage.getItem(k)) || []; }
 function SS(k,v){ localStorage.setItem(k,JSON.stringify(v)); }
 
-// --- Datos principales ---
-let equipos = LS('equipos');        // Array de equipos {nombre, jugadores:[]}
-let partidos = LS('partidos');      // Array de partidos {a, b, acta}
-let clasificacion = LS('clasificacion'); // Calculada
+let equipos = LS('equipos');
+let partidos = LS('partidos');
+let clasificacion = LS('clasificacion');
 
-// --- Inicialización ---
 if(!localStorage.getItem('equipos')) SS('equipos', []);
 if(!localStorage.getItem('partidos')) SS('partidos', []);
 if(!localStorage.getItem('clasificacion')) SS('clasificacion', []);
 
-// Guardar cambios
 function guardarDatos(){
   SS('equipos', equipos);
   SS('partidos', partidos);
